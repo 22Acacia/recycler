789691954f46f0bb3325ad0c0845bd7a209e8c58
pat <pat@22acacia.com>
2015-12-30 23:33:33 -0800

    unit tests pass now, hooray!
    doesn't seem to work in production, but I'm having a hard time
    debugging anything up there.

--- src/main/java/com/acacia/recycler/Recycler.java
+++ src/main/java/com/acacia/recycler/Recycler.java
@@ -12,16 +12,15 @@ import com.acacia.sdk.AbstractTransformComposer;
 import com.google.auto.service.AutoService;
 import com.google.cloud.dataflow.sdk.transforms.DoFn;
 import org.json.JSONObject;
-import org.json.JSONArray;
 
 @AutoService(AbstractTransform.class)
 public class Recycler extends AbstractTransform  {
     private static int MAX_ERROR_COUNT = 5;
-
-
+    
     @Override
     public String transform(String s) {
         JSONObject message = new JSONObject(s);
+        
         int error_count = message.optInt("error_count") + 1;
         if (error_count >= MAX_ERROR_COUNT) {
             //  add a write somewhere later.  make sure the rest works first
@@ -29,12 +28,8 @@ public class Recycler extends AbstractTransform  {
         }
         
         message.put("error_count", error_count);
-        
         //  keep track of being here
-        JSONArray full_path = new JSONArray();
-        full_path = message.optJSONArray("path");
-        full_path.put(full_path.length(), "recycler");
-        message.put("path", full_path);
+        message.append("path", "recycler");
         
         return message.toString();
     }
--- src/test/java/com/acacia/recycler/RecyclerTest.java
+++ src/test/java/com/acacia/recycler/RecyclerTest.java
@@ -3,6 +3,8 @@ package com.acacia;
 import junit.framework.Test;
 import junit.framework.TestCase;
 import junit.framework.TestSuite;
+import com.acacia.recycler.Recycler;
+import org.json.JSONObject;
 
 /**
  * Unit test for simple App.
@@ -10,6 +12,7 @@ import junit.framework.TestSuite;
 public class RecyclerTest 
     extends TestCase
 {
+    protected Recycler testRecycler;
     /**
      * Create the test case
      *
@@ -28,11 +31,21 @@ public class RecyclerTest
         return new TestSuite( RecyclerTest.class );
     }
 
+    protected void setUp() 
+    {
+        testRecycler = new Recycler();
+    }
+    
     /**
      * Rigourous Test :-)
      */
     public void testRecycler()
     {
-        assertTrue( true );
+        String output = testRecycler.transform("{\"forty\":\"hives\"}");
+        System.out.println(output);
+        JSONObject oJson = new JSONObject(output);
+        assertEquals("length is off", oJson.length(), 3);
+        assertEquals("error_count is off", oJson.getInt("error_count"), 1);
+        assertEquals("forty value is off", oJson.getString("forty"), "hives");
     }
 }
